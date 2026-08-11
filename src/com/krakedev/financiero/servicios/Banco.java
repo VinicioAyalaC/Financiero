c6a104108fad4cb563cdd307557c4caa2cde6647
VinicioAyalaC <vhac2311@gmail.com>
2026-08-10 21:10:24 -0500

    05 - Paquete de servicios

--- src/com/krakedev/financiero/servicios/Banco.java
+++ src/com/krakedev/financiero/servicios/Banco.java
@@ -1,5 +1,32 @@
 package com.krakedev.financiero.servicios;
 
 public class Banco {
+	
+	// Atributos
+	private int ultimoCodigo = 1000;
+
+	
+	
+	
+	
+	// Constructor
+	public Banco() {
+	
+	}
+	
+	
+	
+	
+	// Getters & Setters
+	public int getUltimoCodigo() {
+		return ultimoCodigo;
+	}	
+
+	public void setUltimoCodigo(int ultimoCodigo) {
+		this.ultimoCodigo = ultimoCodigo;
+	}
+	
+	
+	
 
 }
